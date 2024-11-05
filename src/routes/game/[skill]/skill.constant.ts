5f5845ce408746df0e7776642614d54be04c167b
Kyle Truong <ktruong008@gmail.com>
2024-11-05 12:05:57 -0500

    Add config for mining and crafting

--- src/routes/game/[skill]/skill.constant.ts
+++ src/routes/game/[skill]/skill.constant.ts
@@ -34,14 +34,62 @@ export const SKILLS_CONFIG = {
       }
     },
     mining: {
-      compactSnow: {},
-      ice: {},
-      blueIce: {}
+      compactSnow: {
+        name: 'Compact snow',
+        requirements: {
+          level: 1
+        },
+        baseTime: 10,
+        baseXp: 4,
+        image: GreenlandCockle
+      },
+      ice: {
+        name: 'Ice',
+        requirements: {
+          level: 5
+        },
+        baseTime: 20,
+        baseXp: 12,
+        image: GreenlandCockle
+      },
+      blueIce: {
+        name: 'Blue ice',
+        requirements: {
+          level: 25
+        },
+        baseTime: 30,
+        baseXp: 32,
+        image: GreenlandCockle
+      }
     },
     crafting: {
-      snowMittens: {},
-      iceHelmet: {},
-      tuskBlades: {}
+      snowMittens: {
+        name: 'Snow mittens',
+        requirements: {
+          level: 1
+        },
+        baseTime: 10,
+        baseXp: 10,
+        image: GreenlandCockle
+      },
+      iceHelmet: {
+        name: 'Ice helmet',
+        requirements: {
+          level: 5
+        },
+        baseTime: 20,
+        baseXp: 100,
+        image: GreenlandCockle
+      },
+      tuskBlades: {
+        name: 'Tusk blades',
+        requirements: {
+          level: 10
+        },
+        baseTime: 30,
+        baseXp: 1000,
+        image: GreenlandCockle
+      }
     }
   }
 };
